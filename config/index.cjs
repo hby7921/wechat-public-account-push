69beb13ca5aadb76f5556dff3e9e5dbb1b97cc97
hby7921 <133756746+hby7921@users.noreply.github.com>
2023-05-17 09:00:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,7 +26,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oOVD66bK9EJ9auilsecpUihaQ18U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'sNwTraQghePat_KC-UKQHAgwscOckDTP7KJQTkHzOFQ	',
+      useTemplateId: 'sNwTraQghePat_KC-UKQHAgwscOckDTP7KJQTkHzOFQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-19',
       festivals: [
@@ -60,7 +60,7 @@ const USER_CONFIG = {
     {
       name: '越小越可怜',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oOVD66bK9EJ9auilsecpUihaQ18U	',
+      id: 'oOVD66bK9EJ9auilsecpUihaQ18U',
     }
   ],
 
