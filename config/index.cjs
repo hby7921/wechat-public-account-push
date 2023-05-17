f45f3583c3b93cc3a72eb64b33e489145efcde91
hby7921 <133756746+hby7921@users.noreply.github.com>
2023-05-17 13:47:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,7 +27,7 @@ SWITCH: {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oOVD66bK9EJ9auilsecpUihaQ18U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'sNwTraQghePat_KC-UKQHAgwscOckDTP7KJQTkHzOFQ',
+      useTemplateId: '0LbcqdvMYQu0X3ACOJs2OHsneR76srmQlJU0Sym8_q8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-19',
       
