acb2acb3caa0f8a0b37b04125466b4e107ff5ba4
hby7921 <133756746+hby7921@users.noreply.github.com>
2023-08-29 21:08:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,13 +20,6 @@ SWITCH: {
   earthyLoveWords: true,
   horoscope: true,
 },
-     TIAN_API: {
-    // 天行API KEY，如果使用天行API则需要填写此项
-    key: 'd459d88391e588c09c089874d51d3bdd',
-    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
-    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
-    networkHot: 3,
-  },
   USERS: [
     {
       // 想要发送的人的名字
@@ -34,7 +27,7 @@ SWITCH: {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oOVD66bK9EJ9auilsecpUihaQ18U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ORvynQbsaN9I2laMMe8M9Uxqr78vHb1O95P-PHHknj8',
+      useTemplateId: 'Z2T9fY7MQs3ZPLoyFNrCTFdxd2aXzKlV6h3cO60Oeyg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-19',
         festivals: [
@@ -44,9 +37,6 @@ SWITCH: {
             {
       type: '节日', name: '亲亲老公生日', year: '2002', date: '01-04',
     }
-            {
-          type: '节日', name: '我们的纪念日', year: '2023', date: '05-14',
-        },
       ],
        
       
