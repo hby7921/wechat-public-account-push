5b96b2513f52595d3e9c0e67b9ee26c7b939e617
hby7921 <133756746+hby7921@users.noreply.github.com>
2023-08-29 21:43:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,9 +25,9 @@ SWITCH: {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oOVD66bK9EJ9auilsecpUihaQ18U',
+      id: 'oOVD66UrbRH_iFxH2PbOFlUy3wy0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'lMrMlvPjPSpaWpXIcsEdjUoR2wEGAIzYhJNF1IZ99Ls',
+      useTemplateId: 'MjoIoIBRYbtdKtMESdQof1Is8d4DDHvXUJpQ5F7qIFA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-19',
         festivals: [
